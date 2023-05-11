70cb1296053c9812abebc8466298297fc01794d4
[Graham Synnott] <[graham.synnott@linamar.com]>
2023-05-10 20:03:09 -0400

    Initial setup of file structure and added code

--- client/src-sw.js
+++ client/src-sw.js
@@ -7,6 +7,31 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
+const cacheName = 'static-resources';
+const matchCallback = ({ request }) => {
+  console.log(request);
+  return (
+    // CSS
+    request.destination === 'style' ||
+    // JavaScript
+    request.destination === 'script' ||
+    // Web Workers
+    request.destination === 'worker'
+  );
+};
+
+registerRoute(
+  matchCallback,
+  new StaleWhileRevalidate({
+    cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
